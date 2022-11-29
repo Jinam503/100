f7ed665be4e6dc4d86ca18ab3b7d4730398e942f
Jinam503 <jinam1234567@gmail.com>
2022-11-29 16:38:26 +0900

    221129

---
+++ 10807.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, arr[100], m, count = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cin >> m;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == m){
+            count++;
+        }
+    }
+    cout << count << endl;
+    
+    return 0;
+}
